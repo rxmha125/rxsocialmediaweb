4d92750c32e7654a5efed58e8ed136ae165c3710
Rx MHA <rz.mha2@gmail.com>
2025-01-30 20:32:49 +0600

    project compleated

---
+++ Desktop/rxsocialmedia/src/app/layout.tsx
@@ -0,0 +1,63 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Rx Social Media",
+  description: "Rx Social Media is a social media platform built with Next.js and Prisma.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+           <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="min-=h-screen">
+              <Navbar />
+
+              <main className="py-8">
+                  <div className="max-w-7xl mx-auto px-4">
+                      <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                          <div className="hidden lg:block lg:col-span-3">
+                            <Sidebar />
+                          </div>
+                          <div className="lg:col-span-9">
+                              {children}
+                          </div>
+                      </div>
+                  </div>
+              </main>
+            </div>
+        </ThemeProvider>
+      </body>
+    </html>
+     </ClerkProvider>
+  );
+}
